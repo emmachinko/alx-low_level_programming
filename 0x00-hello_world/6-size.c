3f556a4af9702f822e5a2a6d617c31b7fdc51de3
emmachinko <emmanuelchris4603@gmail.com>
2023-09-07 18:57:02 -0700

    Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,11 +10,11 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("size of a char:%d byte(s)\n", sizeof(char));
-printf("size of an int:%d byte(s)\n", sizeof(intel));
-printf("size of a long int:%d byte(s)\n", sizeof(long intel));
-printf("size of a long long int:%d byte(s)\n", sizeof(long long intel));
-printf("size of a float:%d byte(s)\n", sizeof(float));
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of an int: %d byte(s)\n", sizeof(intel));
+printf("size of a long int: %d byte(s)\n", sizeof(long intel));
+printf("size of a long long int: %d byte(s)\n", sizeof(long long intel));
+printf("size of a float:%d  byte(s)\n", sizeof(float));
 return (0);
 }
 	
