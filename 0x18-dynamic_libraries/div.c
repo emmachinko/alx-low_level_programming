112385b79c9834da1171dd12d5569a3f2ca64257
emmachinko <emmanuelchris4603@gmail.com>
2023-12-15 20:04:46 -0800

    C - Dynamic libraries

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * div - divide two integers
+ * @a: first int
+ * @b: second int
+ * Return: divide final result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * sub - sub two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtract final result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
